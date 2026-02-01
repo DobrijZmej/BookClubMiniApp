7cac4ad1d466b908be230927c9a311fba2daf968
Zmij <DobrijZmej@gmail.com>
2026-02-01 20:55:51 +0200

    Fix for float model type

--- backend/app/models/db_models.py
+++ backend/app/models/db_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text, Enum, Boolean, Index
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text, Enum, Boolean, Index, Float
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.database import Base
